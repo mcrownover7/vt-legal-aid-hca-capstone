03cc0836c292b417409f056898af5c1b84aa4092
Matthew Crownover <matthew.j.crownover@gmail.com>
2021-11-24 15:47:55 -0500

    Removing extra state boundary. Updating Map.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,26 @@
 //imports
 import { MapContainer, TileLayer, Polygon, GeoJSON } from "react-leaflet";
-import borderData from "../data/border.js";
-import countyBoundary from "../data/countyBorder.js"
+import countyBoundary from "../data/countyBorder.js";
 
 function Map(props) {
-  let vtOutline = borderData.geometry.coordinates[0].map((coords) => [
-    coords[1],
-    coords[0],
-  ]);
-
   return (
     <MapContainer
       center={props.center}
       zoom={8}
       scrollWheelZoom={false}
       doubleClickZoom={false}
-      zoomControl={true}
+      zoomControl={false}
       touchZoom={false}
-      dragging={true}
+      dragging={false}
+      zoomDelta={0}
+      keyboard={false}
       style={{ height: "600px", width: "600px" }}
     >
-   <TileLayer
+      <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <GeoJSON data={countyBoundary} />
+      <GeoJSON data={countyBoundary} style={{fillColor: '#ff6863', fillOpacity: 0.5, color: 'black', weight: 1, opacity: 1,  }} />
     </MapContainer>
   );
 }
