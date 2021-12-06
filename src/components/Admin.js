41cbf25100e651b8fc6cefe8e8366744cc94bb29
Michael Pendleton <Pendletonm@gmail.com>
2021-12-06 16:46:57 -0500

    installed mui grid, generated mui grid on update button, next step to work in delete

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -8,10 +8,13 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import { Menu as MenuIcon } from "@material-ui/icons";
+import { DataGrid } from "@mui/x-data-grid";
 
 export default function Admin(props) {
   const [allStories, setAllStories] = useState([]);
   const [createStoryForm, setCreateStoryForm] = useState(false);
+  const [updateStory, setUpdateStory] = useState(false);
+  const [rowSelection, setRowSelection] = useState([]);
 
   //useEffect to fetch all stories once on component render
   useEffect(() => {
@@ -26,9 +29,61 @@ export default function Admin(props) {
 
   function openStoryForm() {
     setCreateStoryForm(true);
-
+    console.log(allStories);
     return null;
   }
+
+  function openUpdate() {
+    setUpdateStory(true);
+    return null;
+  }
+
+  // function updateSelectedRow() {
+  //   setRowSelection(updateSelectedRows.rows);
+  //   console.log(rowSelection);
+  // }
+
+  function deleteEntry() {
+    console.log(rowSelection)
+  }
+
+  const columns = [
+    {
+      field: "RespID",
+      headerName: "Response ID",
+      width: 120,
+      stretchable: true,
+    },
+    { field: "County", headerName: "County", width: 110 },
+    { field: "Insured", headerName: "Insured", width: 70 },
+    { field: "Age", headerName: "Age", width: 70 },
+    {
+      field: "HowHasMedicalDebtImpactedYourLife",
+      headerName: "Life Impact?",
+      width: 500,
+      resizable: true,
+    },
+    {
+      field: "HowHasMedicalDebtImpactedYourAccessToCare",
+      headerName: "Care Access Impact?",
+      width: 500,
+    },
+    {
+      field: "WhatDoYouThinkOfTheCostOfMedicalCare",
+      headerName: "About Cost of Care?",
+      width: 500,
+    },
+    {
+      field: "HaveYouBeenSurprisedByAMedicalBill",
+      headerName: "Experienced Surprise?",
+      width: 500,
+    },
+    {
+      field: "WhatIsYourExperienceWithMedicalDebtCollectors",
+      headerName: "Experienced Debt Collectors?",
+      width: 500,
+    },
+  ];
 
   return (
     <>
@@ -71,6 +126,38 @@ export default function Admin(props) {
               />
               <input type="submit" />
             </form>,
+          ]
+        : null}
+
+      <Button color="inherit" onClick={openUpdate}>
+        Update Stories
+      </Button>
+
+      {updateStory
+        ? [
+            <div style={{ height: 600, width: "100%" }}>
+              <DataGrid
+                rows={allStories}
+                columns={columns}
+                rowsPerPageOptions={[10]}
+                getRowId={(row) => row._id}
+                onSelectionModelChange={(newSelectionModel) => {
+                  setRowSelection(newSelectionModel);
+                  console.log(newSelectionModel)
+                }}
+                rowSelection={rowSelection}
+              />,
+            </div>,
+            <div>
+              <Button color="inherit" onClick={openUpdate}>
+                Update Story
+              </Button>
+            </div>,
+            <div>
+              <Button color="inherit" onClick={deleteEntry}>
+                Delete Story
+              </Button>
+            </div>,
           ]
         : null}
     </>
