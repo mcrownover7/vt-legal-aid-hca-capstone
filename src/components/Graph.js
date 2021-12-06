ac97fee77dde56c2762afa834effacf2b072531d
Paul <p.dodson@cableone.com>
2021-12-06 16:32:19 -0500

    front page layout, boarder on map, feature story fomatting, and place holder for graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import Graph1 from '../Graph1.png' 
 
-function Graph () {
- 
-  return (
-    <img
-      src={Graph1}
-      alt='graph'
-      width='550'
-      height='300'
-    />
-  )
+import React from "react";
+import Graph1 from "../Graph1.png";
+
+function Graph() {
+  return <img src={Graph1} alt="graph" width="600" height="300" />;
+
 }
 
-export default Graph
+export default Graph;
