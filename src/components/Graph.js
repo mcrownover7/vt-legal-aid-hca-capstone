244fd038fe43e45375edb3a95927b402b60ef1f4
Terri Isidro <terri.isidro@gmail.com>
2021-12-06 15:52:44 -0500

    front page layout work

---
+++ src/components/Graph.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Graph1 from '../Graph1.jpg' 
+
+function Graph () {
+ 
+  return (
+    <img
+      src={Graph}
+      alt='graph'
+      width='300'
+      height='300'
+    />
+  )
+}
+
+export default Graph
