ad36d12b4f77ea3ec517fce5c2f254e29512f609
Terri Isidro <terri.isidro@gmail.com>
2022-01-25 19:47:28 -0500

    forgot to remove references to admin portal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Map from "./components/Map";
-import Home from "./components/Home";
-import About from "./components/About";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Map from './components/Map'
+import Home from './components/Home'
+import About from './components/About'
 
-// import Admin from "./components/Admin";
-import AdminPortalLogin from "./components/AdminPortalLogin";
+// // import Admin from "./components/Admin";
+// import AdminPortalLogin from "./components/AdminPortalLogin";
 
 //setting up routes using react router
-function App(props) {
+function App (props) {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admin-portal" element={<AdminPortalLogin />} />
-          <Route path="/map" element={<Map />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          {/* <Route path="/admin-portal" element={<AdminPortalLogin />} /> */}
+          <Route path='/map' element={<Map />} />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
