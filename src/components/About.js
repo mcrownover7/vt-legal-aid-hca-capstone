c3821b61d5511c53806250673f341c4c26550bcf
Terri Isidro <terri.isidro@gmail.com>
2022-01-25 19:36:25 -0500

    site for deployment to heroku, no admin portal, updates to About Us page but formatting needed

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,26 @@ export default function About () {
   return (
     //React fragment (instead of <div>)
     <>
-    <NavAbout />
+      <NavAbout />
       <div className='wrapper'>
-        <div className='left'>left content stuff goes here</div>
-        <main>Here's some stuff about us</main>
+        <main>
+          The Office of the Health Care Advocate (HCA) is collecting Vermonters’
+          experiences with medical debt. We heard from hundreds of Vermonters
+          from all parts of the state who have or continue to suffer from the
+          impact of medical debt. The project has three goals:
+          <p>
+            Inform policy makers and the public about the issue of medical debt
+            and its negative impact on goal of Vermonters getting the right care
+            at the right time. Support the passage of meaningful policy changes
+            to reduce medical debt and improve access to health care for
+            Vermonters and Vermont children and families. Let Vermonters be
+            heard.
+          </p>
+          <p>
+            The project is ongoing and we would love to hear about your
+            experiences. Share your story.
+          </p>
+        </main>
       </div>
     </>
   )
