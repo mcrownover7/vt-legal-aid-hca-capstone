bc42c3ea843b42bc327238c8370ebcbe02ee1170
Terri Isidro <terri.isidro@gmail.com>
2022-02-09 16:32:36 -0500

    Forgot to import NavVerticalAbout on About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import '../App.css'
 import NavAbout from './NavAbout'
-import NavVertical from './NavVertical'
+import NavVerticalAbout from './NavVerticalAbout'
 import { useState, useEffect } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
