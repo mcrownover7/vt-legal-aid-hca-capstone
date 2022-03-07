2b2546264f2597a8b47a437f68c9ff780496e3b2
Terri Isidro <terri.isidro@gmail.com>
2022-03-07 16:33:51 -0500

    trying to fix About page 'cannot GET' upon refresh issue by adding route to index.html on server.js

--- server.js
+++ server.js
@@ -68,6 +68,15 @@ database.on('error', console.error.bind(console, 'connection error'))
 const upload = multer({ dest: 'uploads/' })
 
 //-------------------------------ROUTES----------------------------------------
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 //---------CREATE-------------
 app.post('/createnew', async (req, res) => {
   //creation of a new story from admin portal
